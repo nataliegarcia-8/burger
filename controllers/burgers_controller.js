b4e451dfa8906ad371580b0d8c472f998a2c8098
Natalie Garcia <gnatalie.booking@gmail.com>
2020-09-08 17:03:18 -0400

    all files and folders created. basic layout added

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,28 @@
 const express = require("express");
-const burgers = require("../models/burger.js")
+const router = express.Router();
+
+//importing model to use database functions
+const burger = require("../models/burger.js")
+
+//GET route
+router.get("/", (req, res) => {
+    burger.all((data) => {
+const hbsObject = {
+    burgers: data
+};
+console.log(hbsObject);
+res.render("index", hbsObject);
+    });
+});
+
+//POST route
+router.post("/api/burgers", (req, res) => {
+    burger.create([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured
+    ], (result) => {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
