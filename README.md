fb997844f213c682a0e61bbcaf5a3318336fb32a
Natalie Garcia <gnatalie.booking@gmail.com>
2020-09-12 13:37:19 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# burger
+# Burger Organizer 
+
+## Table of Contents
+* [Description](#description)
+* [Usage](#usage)
+* [Links](#links)
+* [Questions](#questions)
+
+## Description
+An application that allows the user to keep track of their favorite burgers and devour them! The user inputs a burger of their choice and the burger is displayed on the burger list with an option to devour it! Once the user devours the burger by clicking on the devour button, the burger more to the devoured list. 
+
+
+## Usage
+This application can be used to organize and maintain a burger list and if they have been devoured.  
+
+## Links
+Github Repo: https://github.com/nataliegarcia-8/burger
+
+Heroku Link: 
+
+## Questions 
+### If you have any questions please contact me at:
+Email: gnatalie.booking@gmail.com
+
+Github: https://github.com/natalie-garcia-8
--- config/orm.js
+++ config/orm.js
@@ -12,29 +12,6 @@ const orm = {
         const q = "INSERT INTO burgers (burger_name, devoured) VALUES (?, false)";
         connection.query(q, [new_burger], callback)
         },
-    // insertOne: (table, objValues, callback) => {
-        /*
-        var objValues = {
-            burger_name: 'Cheeseburger',
-            devoured: false
-        }
-        Object.keys(objValues) === ['burger_name', 'devoured']
-        Object.values(objValues) === ['Cheeseburger', 'false']
-
-        ['burger_name', 'devoured'].map(x => '?') === ['?', '?']
-        ['?', '?'].toString() === '?,?'
-        */
-        // const q = 'INSERT INTO ?? \
-        //             ('+ Object.keys(objValues).toString() + ') \
-        //             VALUES ('+ Object.keys(objValues).map(x => '?').toString() + ')'
-
-        /*
-        q = 'INSERT INTO ?? (burger_name, devoured) VALUES (?,?)
-        data = ['burgers'].concat(['Cheeseburger', 0])
-        data = ['burgers', 'Cheeseburger', 0)
-        */
-        // const data = [table].concat(Object.values(objValues))
-        // connection.query(q, data, callback)
     updateOne: (id, callback) => {
         const q = 'UPDATE burgers SET devoured = true WHERE id = ?'
         connection.query(q, [id], callback)
